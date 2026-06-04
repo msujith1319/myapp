edcf1d91d8f6c892b3018799adac88b9c6bdd33e
Mypati Sujith <sujith@Mypatis-MacBook-Pro.local>
2026-06-04 18:10:19 +0100

    first commit

---
+++ backend/config/utils.js
@@ -0,0 +1,11 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+const PORT = process.env.PORT;
+const MONGODB_URI = process.env.MONGODB_URI;
+const REDIS_URL = process.env.REDIS_URL;
+
+console.log("MONGODB_URI =", process.env.MONGODB_URI);
+console.log("REDIS_URL =", process.env.REDIS_URL);
+
+export { MONGODB_URI, PORT, REDIS_URL };
